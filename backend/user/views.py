3960a503c42aa8b2e591baf6352e9370ce8c4201
Daniil.Diveev <Daniil.Diveev@x5.ru>
2024-07-17 19:16:35 +0300

    wip

--- backend/user/views.py
+++ backend/user/views.py
@@ -50,10 +50,13 @@ class RegisterView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        refresh = RefreshToken.for_user(user)
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        })
-
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+        
 class LoginView(generics.GenericAPIView):
     permission_classes = [AllowAny]
 

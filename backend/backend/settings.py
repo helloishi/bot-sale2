889f5edd1cfa829732e29d8a759e424643a762ed
Daniil.Diveev <Daniil.Diveev@x5.ru>
2024-07-20 00:09:49 +0300

    wip

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,7 +81,7 @@ CORS_ALLOW_CREDENTIALS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

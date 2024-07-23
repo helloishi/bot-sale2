fc95999ee1d1c9152188c40181d5cd26692eeb14
Daniil.Diveev <Daniil.Diveev@x5.ru>
2024-07-23 17:19:00 +0300

    wip

--- restart_back.sh
+++ restart_back.sh
@@ -1,6 +1,7 @@
 echo "Pulling from git.."
 echo "-------------------------"
 git pull origin backend
+echo "-------------------------"
 echo "Recreating service..."
 echo "-------------------------"
 docker compose up web --build -d --no-deps --force-recreate

eb08080526017a35e05413587fa8716c0a3b0480
Daniil.Diveev <Daniil.Diveev@x5.ru>
2024-08-22 14:32:59 +0300

    wip

--- backend/discounts/serializers.py
+++ backend/discounts/serializers.py
@@ -7,16 +7,20 @@ class FavoriteDiscountSerializer(serializers.Serializer):
 
 class DiscountSerializer(serializers.ModelSerializer):
     is_favorite = serializers.SerializerMethodField()
+    place_type_display = serializers.SerializerMethodField()
 
     class Meta:
         model = Discount
-        fields = ['id', 'place', 'image', 'description', 'address_txt', 'start_date', 'end_date', 'place_type', 'is_active', 'is_favorite']
+        fields = ['id', 'place', 'image', 'description', 'address_txt', 'start_date', 'end_date', 'place_type', 'is_active', 'is_favorite', 'place_type_display']
 
     def get_is_favorite(self, obj):
         user = self.context.get('user')
         if user:
             return obj in user.fav_discounts.all()
         return False
+
+    def get_place_type_display(self, obj):
+        return obj.get_place_type_display()
 
 class UserSerializer(serializers.ModelSerializer):
     fav_discounts = DiscountSerializer(many=True, read_only=True)
